d1f9fab4ad47494d38b1cddf08a9ee87d1ae3100
jasncani <jasonbacani@gmail.com>
2017-12-02 23:32:14 +1000

    refactor code

--- game.js
+++ game.js
@@ -1,9 +1,6 @@
 var character;
 var obstacles = [];
-var keys = [];
-keys["spacebar"] = 32;
-keys["w"] = 87;
-keys['upArrow'] = 38;
+var keys = {spacebar : 32, w: 87, upArrow: 38};
 var xSpeed = 3;
 var ySpeed = 3;
 var spawnInterval = 150;
@@ -13,21 +10,22 @@ var gameFps = 120;
 var canvasWidth = 720;
 var canvasHeight = 240;
 
+// the canvas object
 var gameArea = {
   canvas : document.createElement("canvas"),
   start : function() { // create a <canvas> html element
-    this.canvas.width = 720;
-    this.canvas.height = 240;
+    this.canvas.width = canvasWidth;
+    this.canvas.height = canvasHeight;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
     this.interval = setInterval(updateGameArea, 1000 / gameFps); // call updateGameArea on an interval
     window.addEventListener('keydown', function (e) {
       gameArea.key = e.keyCode;
-    })
+    });
     window.addEventListener('keyup', function (e) {
       gameArea.key = false;
-    })
+    });
   },
   clear : function() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -41,26 +39,30 @@ function updateGameArea() {
   gameArea.clear();
   gameArea.frameNo += 1;
   if (gameArea.frameNo == 1 || framesPassed(spawnInterval)) {
-    var obstacleHeight = (Math.random() * characterHeight) + characterHeight;
-    var x = gameArea.canvas.width;
-    var y = gameArea.canvas.height - obstacleHeight;
-    obstacles.push(new component(characterWidth, obstacleHeight, "green", x, y));
+    createNewObstacle();
   }
   for (i = 0; i < obstacles.length; i += 1) {
     obstacles[i].x += -xSpeed;
     obstacles[i].update();
   }
   character.update();
-  if (gameArea.key && (gameArea.key == keys["spacebar"] || gameArea.key == keys["upArrow"] || gameArea.key == keys["w"])) {
+  if (gameArea.key && (gameArea.key == keys.spacebar || gameArea.key == keys.upArrow || gameArea.key == keys.w)) {
     character.y = gameArea.canvas.height - 4 * characterHeight;
   }
   else { character.y = gameArea.canvas.height - characterHeight; }
   for (i = 0; i < obstacles.length; i += 1) {
-    if (character.crashWith(obstacles[i])) {
+    if (character.colidesWith(obstacles[i])) {
       gameArea.stop();
       return;
     }
   }
+}
+
+function createNewObstacle() {
+  var obstacleHeight = (Math.random() * characterHeight) + characterHeight;
+  var x = gameArea.canvas.width;
+  var y = gameArea.canvas.height - obstacleHeight;
+  obstacles.push(new component(characterWidth, obstacleHeight, "green", x, y));
 }
 
 function framesPassed(n) {
@@ -68,6 +70,7 @@ function framesPassed(n) {
   return false;
 }
 
+// the character/obstacle object
 function component(width, height, color, x, y) {
   this.width = width;
   this.height = height;
@@ -78,22 +81,21 @@ function component(width, height, color, x, y) {
     ctx = gameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-  this.crashWith = function(otherobj) {
-    var myleft = this.x;
-    var myright = this.x + (this.width);
-    var mytop = this.y;
-    var mybottom = this.y + (this.height);
-    var otherleft = otherobj.x;
-    var otherright = otherobj.x + (otherobj.width);
-    var othertop = otherobj.y;
-    var otherbottom = otherobj.y + (otherobj.height);
-    var crash = true;
-    if ((mybottom < othertop) || (mytop > otherbottom) || (myright < otherleft) || (myleft > otherright)) {
-      crash = false;
+  };
+  this.colidesWith = function(otherObj) {
+    var myLeft = this.x;
+    var myRight = this.x + (this.width);
+    var myTop = this.y;
+    var myBottom = this.y + (this.height);
+    var otherLeft = otherObj.x;
+    var otherRight = otherObj.x + (otherObj.width);
+    var otherTop = otherObj.y;
+    var otherBottom = otherObj.y + (otherObj.height);
+    if ((myBottom < otherTop) || (myTop > otherBottom) || (myRight < otherLeft) || (myLeft > otherRight)) {
+      return false;
     }
-    return crash;
-  }
+    return true;
+  };
 }
 
 function startGame() {
