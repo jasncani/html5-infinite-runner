65f852964b214ce11004d7b389a9ac2262848e19
jasncani <jasonbacani@gmail.com>
2017-12-10 17:35:41 +1000

    refactor code

--- test.js
+++ test.js
@@ -1,5 +1,11 @@
+var GRAVITY = 10;
+var CANVAS_WIDTH = 480;
+var CANVAS_HEIGHT = 240;
+var KEYS = {SPACEBAR : 32, W: 87, UP_ARROW: 38};
+var CHARACTER_WIDTH = 56;
+var CHARACTER_HEIGHT = 77;
+
 function sprite (options) {
-  this.context = options.context;
   this.width = options.width;
   this.height = options.height;
   this.image = options.image;
@@ -10,7 +16,7 @@ function sprite (options) {
   this.yMin = options.yMin;
   this.frameIndex = 1;
   this.render = function () {
-    this.context.drawImage(
+    game.context.drawImage(
       this.image,                               // The image sprite sheet
       this.frameIndex * this.width,             // The x coordinate where to start clipping on the sprite sheet
       0,                                        // The y coordinate where to start clipping on the sprite sheet
@@ -23,52 +29,55 @@ function sprite (options) {
     );
   };
   this.update = function() {
-    if (this.y == canvas.height - this.height) { this.frameIndex = (this.frameIndex + 1) % this.numFrames; }
-    this.y += this.yVelocity;
-    if (this.y < this.yMin) { this.yVelocity = -1 * this.yVelocity; }
-    if (this.y > canvas.height - this.height) {
-      this.y = canvas.height - this.height;
-      this.yVelocity = 0;
-    }
+    this.frameIndex = (this.frameIndex + 1) % this.numFrames;
   };
 }
 
 function loop() {
-  if (canvas.key && (canvas.key == keys.spacebar || canvas.key == keys.upArrow || canvas.key == keys.w) && character.y == canvas.height - character.height) {
+  if (game.key && (game.key == KEYS.SPACEBAR || game.key == KEYS.UP_ARROW || game.key == KEYS.W) && character.y == game.canvas.height - character.height) {
     character.yVelocity = -12;
   }
   character.update();
-  character.context.clearRect(0, 0, canvas.width, canvas.height);
+  character.y += character.yVelocity;
+  if (character.y < character.yMin) { character.yVelocity = -1 * character.yVelocity; }
+  if (character.y > game.canvas.height - character.height) {
+    character.y = game.canvas.height - character.height;
+    character.yVelocity = 0;
+  }
+  game.context.clearRect(0, 0, game.canvas.width, game.canvas.height);
   character.render();
 }
 
-var keys = {spacebar : 32, w: 87, upArrow: 38};
-
-var canvas = document.getElementById("charTestCanvas");
-canvas.width = 480;
-canvas.height = 240;
-window.addEventListener('keydown', function (e) {
-  canvas.key = e.keyCode;
-});
-window.addEventListener('keyup', function (e) {
-  canvas.key = false;
-});
+var game = {
+  canvas: document.getElementById("charTestCanvas"),
+  start: function() {
+    this.context = this.canvas.getContext("2d");
+    this.canvas.width = CANVAS_WIDTH;
+    this.canvas.height = CANVAS_HEIGHT;
+    this.interval = setInterval(loop, 70);
+    window.addEventListener('keydown', function (e) {
+      game.key = e.keyCode;
+    });
+    window.addEventListener('keyup', function (e) {
+      game.key = false;
+    });
+  }
+};
 
 var characterImage = new Image();
 characterImage.src = "running-man-animation-sprite-8-frame-loop.png";
 
 var character = new sprite(
   {
-    context: canvas.getContext("2d"),
-    width: 56,
-    height: 77,
+    width: CHARACTER_WIDTH,
+    height: CHARACTER_HEIGHT,
     image: characterImage,
     numFrames: 8,
     x: 0,
-    y: canvas.height - 77,
+    y: CANVAS_WIDTH - CHARACTER_HEIGHT,
     yVelocity: 0,
-    yMin: canvas.height - 1.5 * 77
+    yMin: CANVAS_HEIGHT - 1.5 * CHARACTER_HEIGHT
   }
 );
 
-canvas.interval = setInterval(loop, 50);
+game.start();
